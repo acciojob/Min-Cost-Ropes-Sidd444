5ea8756eb3a4ce86297d5be7c42c912865adcba3
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-11 14:42:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  const h=new minHeap(arr);
+  let total=0;
+  while(h.length>1){
+	  let a=h.findMin(),b=h.findMin();
+	  let sum=a+b;
+	  total+=sum;
+	  h.push(sum);
+  }
+  total+=h.getMin();
+  return total;
 }
-
+class minHeap{
+	const heap=[];
+	constructor(arr){
+		for(let a in arr) heap.push(a);
+	}
+	function findMin(){
+		heap.sort((x,y)=>x-y);
+		let m=heap.pop();
+		return m;
+	}
+}
 module.exports=mincost;
